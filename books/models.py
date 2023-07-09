9a6c6b5fc08477dc44ae0a834a3185e571c0f4db
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2023-07-08 19:10:01 -0500

    modelos

--- books/models.py
+++ books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+# Modelo para la entidad TipoDocumento.
+class TipoDocumento(models.Model):
+  nombre = models.CharField(max_length=30)
+  descripcion = models.CharField(max_length=300)
+  
+# Modelo para la entidad Ciudad.
+class Ciudad:
+  nombre = models.CharField(max_length=30)
+  descripcion = models.CharField(max_length=300)
+
+# Modelo para la entidad Empresa.
+class Empresa(models.Model):
+  nombre = models.CharField(max_length=30)
+  nit = models.IntegerField(max_length=10)
+
+# Modelo para la entidad Empleado.
+class Empleado(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  tipo_documento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
+  documento = models.IntegerField(max_length=10)
+  lugar_residencia = models.ForeignKey(Ciudad,on_delete=models.CASCADE)
+  fecha_nacimiento = models.DateField(null=True)
+  email = models.EmailField(max_length=50)
+  telefono = models.IntegerField(max_length=10)
+  usuario = models.CharField(max_length=50)
+  password = models.CharField(max_length=50)
+  empresa = models.ForeignKey(Empresa,on_delete=models.CASCADE)
+
